5c815403ef76b0bdfe4870eb21cf64e58659b59c
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-10-06 18:31:03 +0200

    primer commit de este proyecto

--- src/components/Initial.jsx
+++ src/components/Initial.jsx
@@ -5,67 +5,71 @@ const Initial = () => {
         <div className='my-2 border py-1 text-blue-500  bg-gray-300'>
             <form action="" className='flex flex-col justify-center'>
 
+            <div className='mt-4'>
+                <span className='flex justify-center mb-3'>Datos del Cliente</span>
                 <div className='my-1 px-1 flex justify-between'>
-                    <label htmlFor="client" className="mr-3 text-italic w-1/3">Cliente:</label>
-                    <input type="text" id='client' className='rounded w-2/3 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Nombre del Cliente' />
+                    <label htmlFor="client" className="mr-3 text-italic w-1/4">Cliente:</label>
+                    <input type="text" id='client' className='rounded w-3/4 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Nombre del Cliente' />
                 </div>
 
                 <div className='my-1 px-1 flex justify-between'>
-                    <label htmlFor="cel" className="mr-3 text-italic w-1/3">Movil:</label>
-                    <input type="tel" id='cel' className='rounded w-2/3 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Movil del Cliente' />
+                    <label htmlFor="cel" className="mr-3 text-italic w-1/4">Movil:</label>
+                    <input type="tel" id='cel' className='rounded w-3/4 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Movil del Cliente' />
                 </div>
 
                 <div className='my-1 px-1 flex justify-between'>
-                    <label htmlFor="email" className="mr-3 text-italic w-1/3">Email:</label>
-                    <input type="mail" id='email' className='rounded w-2/3 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='E-mail del Cliente' />
+                    <label htmlFor="email" className="mr-3 text-italic w-1/4">Email:</label>
+                    <input type="mail" id='email' className='rounded w-3/4 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='E-mail del Cliente' />
                 </div>
 
                 <div className='my-1 px-1 flex justify-between items-center'>
-                    <label htmlFor="reception" className="mr-3 text-italic w-1/3">Recepción:</label>
+                    <label htmlFor="reception" className="mr-3 text-italic w-1/4">Recepción:</label>
                     <input type="date" id='reception' className='rounded max-h-7'/>
                 </div>
 
                 <div className='my-1 px-1 flex justify-between items-center'>
-                    <label htmlFor="promise" className="mr-3 text-italic w-1/3">Fecha de Compromiso:</label>
+                    <label htmlFor="promise" className="mr-3 text-italic w-1/4">Compromiso:</label>
                     <input type="date" id='promise' className='rounded max-h-7' />
                 </div>
 
                 <div className='my-1 px-1 flex justify-between items-center'>
-                    <label htmlFor="delivered" className="mr-3 text-italic w-1/3">Entrega:</label>
+                    <label htmlFor="delivered" className="mr-3 text-italic w-1/4">Entrega:</label>
                     <input type="date" id='delivered' className='rounded max-h-7' />
                 </div>
 
                 <div className='my-1 px-1 flex justify-between'>
-                    <label htmlFor="possibleFailure" className="mr-3 text-italic w-1/3">Posible Falla:</label>
-                    <textarea name="textarea" id='client' rows="3" className='rounded w-2/3 placeholder-slate-300 py-0.5 text-justify placeholder:italic' placeholder='Descripción: Posible falla' />
+                    <label htmlFor="possibleFailure" className="mr-3 text-italic w-1/4">Posible Falla:</label>
+                    <textarea name="textarea" id='client' rows="3" className='rounded w-3/4 placeholder-slate-300 py-0.5 text-justify placeholder:italic' placeholder='Descripción: Posible falla' />
                 </div>
 
-                <div className='mt-4 text-center'>
-                <span className='text-center'>Datos del Vehiculo</span>
+</div>
+
+                <div className='mt-4'>
+                <span className='flex justify-center mb-3'>Datos del Vehiculo</span>
 
                 <div className='my-1 px-1 flex justify-between'>
-                    <label htmlFor="brand" className="mr-3 text-italic w-1/3">Marca:</label>
-                    <input type="text" id='brand' className='rounded w-2/3 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Marca del Vehiculo' />
-                </div>
-
-                <div className='my-1 px-1 flex justify-between'>
-                    <label htmlFor="model" className="mr-3 text-italic w-1/3">Modelo:</label>
-                    <input type="text" id='model' className='rounded w-2/3 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Modelo del Vehiculo' />
+                    <label htmlFor="brand" className="mr-3 text-italic w-1/4">Marca:</label>
+                    <input type="text" id='brand' className='rounded w-3/4 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Marca del Vehiculo' />
                 </div>
 
                 <div className='my-1 px-1 flex justify-between'>
-                    <label htmlFor="year" className="mr-3 text-italic w-1/3">Año:</label>
-                    <input type="number" id='year' className='rounded w-2/3 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Año del Vehiculo' />
+                    <label htmlFor="model" className="mr-3 text-italic w-1/4">Modelo:</label>
+                    <input type="text" id='model' className='rounded w-3/4 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Modelo del Vehiculo' />
                 </div>
 
                 <div className='my-1 px-1 flex justify-between'>
-                    <label htmlFor="km" className="mr-3 text-italic w-1/3">Km:</label>
-                    <input type="number" id='km' className='rounded w-2/3 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Kilometraje' />
+                    <label htmlFor="year" className="mr-3 text-italic w-1/4">Año:</label>
+                    <input type="number" id='year' className='rounded w-3/4 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Año del Vehiculo' />
                 </div>
 
                 <div className='my-1 px-1 flex justify-between'>
-                    <label htmlFor="failure" className="mr-3 text-italic w-1/3">Falla:</label>
-                    <textarea name="textarea" id='failure' rows="3" className='rounded w-2/3 placeholder-slate-300 py-0.5 text-justify placeholder:italic' placeholder='Descripción: Falla detectada' />
+                    <label htmlFor="km" className="mr-3 text-italic w-1/4">Km:</label>
+                    <input type="number" id='km' className='rounded w-3/4 placeholder-slate-300 py-0.5 text-center placeholder:italic' placeholder='Kilometraje' />
+                </div>
+
+                <div className='my-1 px-1 flex justify-between'>
+                    <label htmlFor="failure" className="mr-3 text-italic w-1/4">Falla:</label>
+                    <textarea name="textarea" id='failure' rows="3" className='rounded w-3/4 placeholder-slate-300 py-0.5 text-justify placeholder:italic' placeholder='Descripción: Falla detectada' />
                 </div>
 
                 </div>
