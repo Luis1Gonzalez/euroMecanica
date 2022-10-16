c2456d01de65d36060e6d53ba55cb11a44e51ff6
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-10-16 04:04:18 +0200

    aun falta sacar el total

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Link } from 'react-router-dom';
 
@@ -7,35 +7,57 @@ const Budget = ({ cambiazo }) => {
 
   let date = new Date();
   let output = String(date.getDate()).padStart(2, '0') + '/' + String(date.getMonth() + 1).padStart(2, '0') + '/' + date.getFullYear();
+  let count = 0
 
   const { client, cel, mail, reception, promise, possibleFailure, km, failure, delivered, brand, model, id, year, now } = cambiazo
 
   const [details, setDetails] = useState([]);
   const [description, setDescripcion] = useState("");
-  const [cant, setCant] = useState("");
-  const [price, setPrice] = useState("");
+  const [cant, setCant] = useState('');
+  const [price, setPrice] = useState('');
+  const [prod, setProd] = useState('');
 
+
+
+  
   const generateId = () => {
     const dateNow = Date.now().toString(36).toUpperCase();
     return dateNow;
+  }
 
-}
 
   const onSubmit = (e) => {
     e.preventDefault()
 
+
+
     const objDetail = {
       id: generateId(),
       description,
-      cant: cant,
-      price
+      cant,
+      price,
+      prod: cant*price,
+
     }
+
+
+
+
+
+  
     setDetails([...details, objDetail])
 
     setDescripcion('');
     setCant('');
     setPrice('');
+
+
+
   }
+
+
+  
+
 
 
 
@@ -73,15 +95,13 @@ const Budget = ({ cambiazo }) => {
           <p className='w-2/3'>Descripción</p>
           <p className='text-center w-1/4'>Cant.</p>
           <p className='text-center w-1/4'>Precio</p>
-          <p className='text-center w-1/4'>Total</p>
         </div>
 
         <div className='w-full flex justify-between items-center py-1'>
           <input type="text" id="description" className='mr-1 w-2/3 h-5' value={description} onChange={(e) => setDescripcion(e.target.value)} />
           <input type="number" id="cant" className='mx-1 w-1/6 h-5' value={cant} onChange={(e) => setCant(e.target.value)} />
           <input type="number" id="price" className='mx-1 w-1/6 h-5' value={price} onChange={(e) => setPrice(e.target.value)} />
-          <p id="total" className='mx-1 bg-white w-1/6 h-5 flex items-center justify-center'></p>
-          <button type='submit' className='bg-green-300  rounded-full w-5 h-5 text-lg text-white flex items-center justify-center'>+</button>
+          <button type='submit' className='bg-blue-400  rounded-full w-6 h-5 text-lg text-white flex items-center justify-center'>+</button>
         </div>
 
       </form>
@@ -93,7 +113,7 @@ const Budget = ({ cambiazo }) => {
             <p className='mr-1 bg-white w-2/3 h-5 flex items-center'>{d.description}</p>
             <p className='mx-1 bg-white w-1/6 h-5 flex justify-center items-center'>{d.cant}</p>
             <p className='mx-1 bg-white w-1/6 h-5 flex justify-center items-center'>{d.price}</p>
-            <p className='mx-1 bg-white w-1/6 h-5 flex items-center justify-center'></p>
+            <p className='mx-1 bg-white w-1/6 h-5 flex items-center justify-center'>{d.prod}</p>
 
 
 
@@ -107,12 +127,12 @@ const Budget = ({ cambiazo }) => {
       <div className="mt-2 border-2 border-gray-600 mx-2">
         <div className='flex justify-end px-2'>
           <p className='mx-7'>Sub-Total</p>
-          <p>800,00$</p>
+          <p>0,00$</p>
         </div>
 
         <div className='flex justify-end px-2'>
           <p className='mx-7'>Descuento</p>
-          <p>35,00$</p>
+          <p>0,00$</p>
         </div>
 
         <div className='flex justify-end px-2'>
@@ -122,7 +142,7 @@ const Budget = ({ cambiazo }) => {
 
         <div className='flex justify-end px-2'>
           <p className='mx-7'>Total Presupuesto</p>
-          <p>1034,29$</p>
+          <p>0,00$</p>
         </div>
 
       </div>
